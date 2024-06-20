bf71405150b60a19aa8fc00c7fa93eec92815dd3
OMC-NZ <nz.dev@oppomobile.nz>
2024-06-20 17:01:34 +1200

    !Important: Template of 14.1th Version without Main Info

--- src/app/(pages)/(forms)/template.js
+++ src/app/(pages)/(forms)/template.js
@@ -5,7 +5,7 @@ import { ReCaptchaProvider } from 'react-grecaptcha-v3';
 export default function Template({ children }) {
     return (
         <>
-            <ReCaptchaProvider siteKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}>
+            <ReCaptchaProvider siteKey={process.env.RECAPTCHA_SITE_KEY}>
                 {children}
             </ReCaptchaProvider>
 
